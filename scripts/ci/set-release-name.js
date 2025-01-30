04e53affd7880d8b0422f91d8fdabc16e3397b26
Oleksandr Andriienko <oandriie@redhat.com>
2025-01-30 15:20:26 +0200

    hard-code more
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- scripts/ci/set-release-name.js
+++ scripts/ci/set-release-name.js
@@ -81,10 +81,18 @@ async function main() {
   const latestPreReleaseDate = new Date(
     latestPreRelease.published_at,
   ).getTime();
-  if (releaseLine === 'main' || latestReleaseDate > latestPreReleaseDate) {
+  if (
+    releaseLine === 'main' ||
+    releaseLine === '1.35.0' ||
+    latestReleaseDate > latestPreReleaseDate
+  ) {
     if (releaseLine === 'main') {
       console.log(
         `Selected release line is 'main', using Latest Release name ${latestRelease.name}`,
+      );
+    } else if (releaseLine === '1.35.0') {
+      console.log(
+        `Selected release line is '1.35.0', using Latest Release name ${latestRelease.name}`,
       );
     } else {
       console.log(
