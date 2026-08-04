3e3de779b857bf8a767eed67f3d40f5a9de27e61
Oleksandr Andriienko <oandriie@redhat.com>
2026-08-04 12:23:11 +0300

    feat(rbac): use less requests to filter policies
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- workspaces/rbac/plugins/rbac-backend/src/service/enforcer-delegate.ts
+++ workspaces/rbac/plugins/rbac-backend/src/service/enforcer-delegate.ts
@@ -161,20 +161,31 @@ export class EnforcerDelegate implements RoleEventEmitter<RoleEvents> {
 
   async getFilteredPolicy(
     fieldIndex: number,
-    ...filter: string[]
+    ...filter: string[] | string[][]
   ): Promise<string[][]> {
     const tempModel = newModelFromString(MODEL);
 
-    const filterObj: Record<string, string> = { ptype: 'p' };
-    for (let i = 0; i < filter.length; i++) {
-      if (filter[i]) {
-        filterObj[`v${i + fieldIndex}`] = filter[i];
+    let twoLevelFilter: string[][];
+    if (filter.length > 0 && typeof filter[0] === 'string') {
+      twoLevelFilter = [filter as string[]];
+    } else {
+      twoLevelFilter = filter as string[][];
+    }
+
+    const filterArgs: Record<string, string>[] = [];
+    for (const nestedFilter of twoLevelFilter) {
+      const filterObj: Record<string, string> = { ptype: 'p' };
+      for (let i = 0; i < nestedFilter.length; i++) {
+        if (nestedFilter[i]) {
+          filterObj[`v${i + fieldIndex}`] = nestedFilter[i];
+        }
       }
+      filterArgs.push(filterObj);
     }
 
     await (this.enforcer.getAdapter() as FilteredAdapter).loadFilteredPolicy(
       tempModel,
-      [filterObj],
+      filterArgs,
     );
 
     return await tempModel.getPolicy('p', 'p');
@@ -680,15 +691,15 @@ export class EnforcerDelegate implements RoleEventEmitter<RoleEvents> {
     const model = newModelFromString(MODEL);
     let policies: string[][] = [];
     if (roles.length > 0) {
+      const rolePermissionFilter: string[][] = [];
       for (const role of roles) {
-        const filteredPolicy = await this.getFilteredPolicy(
-          0,
-          role,
-          resourceType,
-          action,
-        );
-        policies.push(...filteredPolicy);
+        rolePermissionFilter.push([role, resourceType, action]);
       }
+      const filteredPolicy = await this.getFilteredPolicy(
+        0,
+        ...rolePermissionFilter,
+      );
+      policies.push(...filteredPolicy);
     } else {
       const enforcePolicies = await this.getFilteredPolicy(
         1,
